af9fbf9323ab479815c6bbdcf0dcb38045173445
Janmejay Singh <singh.janmejay@gmail.com>
2023-10-12 12:30:23 +0530

    get golang backtrace links to show up on output channel

--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,35 @@ import * as vscode from 'vscode';
 
 const pathTok = "/";
 
+class FileLine {
+	readonly file: string;
+	readonly line: number;
+
+	constructor(file: string, line: number) {
+		this.file = file;
+		this.line = line;
+	}
+}
+
 class MatchingFile {
 	readonly givenPath: string;
-
 	readonly workspacePath: string;
 
 	constructor(stackTracePath: string, workspacePath: string) {
 		this.givenPath = stackTracePath;
 		this.workspacePath = workspacePath;
+	}
+}
+
+class TraceLine {
+	readonly origStr: string;
+	readonly origFL: FileLine | undefined;
+	readonly matchingPath: string | undefined;
+
+	constructor(origStr: string, origFL: FileLine | undefined, matchingPath: string | undefined) {
+		this.origStr = origStr;
+		this.origFL = origFL;
+		this.matchingPath = matchingPath;
 	}
 }
 
@@ -25,16 +46,16 @@ function findUniquePath(pathParts: string[], min: number, max: number): Thenable
 	return matchingPathsT.then(
 		paths => {
 			if (paths.length === 1) {
-				console.log(`Found unique path ${paths[0]}`);
+				// console.log(`Found unique path ${paths[0]}`);
 				return paths[0].fsPath;
 			} else if (paths.length === 0) {
-				console.log(`Found no path for ${glob} (${min}, ${max})`);
+				// console.log(`Found no path for ${glob} (${min}, ${max})`);
 				if (s >= max - 1) {
 					return "";
 				}
 				return findUniquePath(pathParts, s, max);
 			} else {
-				console.log(`Found too many paths for ${paths} for ${glob} (${min}, ${max})`);
+				// console.log(`Found too many paths for ${paths} for ${glob} (${min}, ${max})`);
 				if (s <= min + 1) {
 					return "";
 				}
@@ -46,9 +67,61 @@ function findUniquePath(pathParts: string[], min: number, max: number): Thenable
 
 function computeAbsPath(givenPath: string): Thenable<MatchingFile> {
 	const parts = givenPath.split(pathTok);
-	console.log(`Starting lookup for ${givenPath}`);
+	//console.log(`Starting lookup for ${givenPath}`);
 	return findUniquePath(parts, 0, parts.length - 1)
 			.then(p => new MatchingFile(givenPath, p));
+}
+
+function makeFileLine(str: string): (FileLine | undefined) {
+	const lineSep = str.lastIndexOf(':');
+
+	if (lineSep === -1 || lineSep === str.length - 1) {
+		return undefined;
+	}
+
+	const colonSuffix = str.substring(lineSep + 1);
+
+	const lineNo = parseInt(colonSuffix, 10);
+	if (isNaN(lineNo)) {
+		return undefined;
+	}
+
+	const fStr = str.substring(0, lineSep);
+	console.log(`Makeing FL: Str=${str} File=${fStr} Line=${lineNo}`);
+	return new FileLine(fStr, lineNo);
+}
+
+function findFileLine(str: string): (FileLine | undefined) {
+	const pathTokIdx = str.indexOf(pathTok);
+	if (pathTokIdx === -1) {
+		return undefined;
+	}
+
+	var pathStartIdx = pathTokIdx;
+	if ((pathTokIdx > 0) && str[pathTokIdx - 1] !== ' ') { // rel path
+		for (var i = pathTokIdx - 1; i >= 0; i--) {
+			if (str[i] === ' ' || str[i] === '\t') {
+				pathStartIdx = i + 1;
+				break;
+			}
+		}
+	}
+	const suffixedPath = str.substring(pathStartIdx);
+	const endIdx = suffixedPath.indexOf(" ");
+	if (endIdx === -1) {
+		return makeFileLine(suffixedPath);
+	} else {
+		return makeFileLine(suffixedPath.substring(0, endIdx));
+	}
+}
+
+function processTraceLine(str: string): Thenable<TraceLine> {
+	const fl = findFileLine(str);
+	if (!fl) {
+		return Promise.resolve(new TraceLine(str, undefined, undefined));
+	}
+	return computeAbsPath(fl.file)
+		.then(p => new TraceLine(str, fl, p.workspacePath));
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -87,6 +160,50 @@ export function activate(context: vscode.ExtensionContext) {
 						);
 					}
 				);
+
+				const golangTrace = `
+goroutine 25 [syscall, 45 minutes, locked to thread]:
+github.com/janmejay/jnigi._Cfunc_CallVoidMethodA(0x7fa934001348, 0x7fa9340026b0, 0x7faa14c1a4f0, 0x0)
+        _cgo_gotypes.go:962 +0x45
+github.com/janmejay/jnigi.callVoidMethodA.func1(0xabc220?, 0x7fa934002880?, 0xc0000d7cf8?, 0xc0000d7d08?)
+        /home/ubuntu/code/sdmain1/src/go/src/rubrik/vendor/github.com/janmejay/jnigi/cwrapper.go:801 +0xa6
+github.com/janmejay/jnigi.callVoidMethodA(0xc000147040?, 0x0?, 0x0?, 0xd8297b?)
+        /home/ubuntu/code/sdmain1/src/go/src/rubrik/vendor/github.com/janmejay/jnigi/cwrapper.go:801 +0x19
+github.com/janmejay/jnigi.(*ObjectRef).CallMethod(0xc0003763a0, 0xc000147040, {0xd8297b, 0x3}, {0xc85700, 0xeb40b8}, {0x0, 0x0, 0x0})
+        /home/ubuntu/code/sdmain1/src/go/src/rubrik/vendor/github.com/janmejay/jnigi/jnigi.go:1098 +0x68e
+rubrik/cqlproxy/sch_test.runSChScalaTestSuite({0xec37b8, 0xc0000400e0}, 0x0?, 0xc000147040)
+        /home/ubuntu/code/sdmain1/src/go/src/rubrik/cqlproxy/sch/sch_test.go:78 +0x15a
+rubrik/cqlproxy/sch_test.TestAllSchStack(0xc000102ea0)
+        /home/ubuntu/code/sdmain1/src/go/src/rubrik/cqlproxy/sch/sch_test.go:1101 +0x1ba
+testing.tRunner(0xc000102ea0, 0xdee870)
+        /usr/local/go/src/testing/testing.go:1576 +0x10b
+created by testing.(*T).Run
+        /usr/local/go/src/testing/testing.go:1629 +0x3ea
+
+goroutine 69 [chan receive, 47 minutes]:
+rubrik/cqlproxy/server.RunProxyServer({0xec37b8?, 0xc0000400e0}, 0x0?, 0x0?, {0xec7620, 0xc000377980}, {0xec7620?, 0xc0003779a0}, 0xc00010f7a0, {0xec1760, ...}, ...)
+        /home/ubuntu/code/sdmain1/src/go/src/rubrik/cqlproxy/server/server.go:159 +0x62a
+rubrik/cqlproxy/testutil.RunBaseLspProxyServer.func2()
+        /home/ubuntu/code/sdmain1/src/go/src/rubrik/cqlproxy/testutil/test_harness.go:604 +0x197
+created by rubrik/cqlproxy/testutil.RunBaseLspProxyServer
+        /home/ubuntu/code/sdmain1/src/go/src/rubrik/cqlproxy/testutil/test_harness.go:603 +0x1ab
+`;
+				Promise
+					.all(golangTrace.split('\n').map(processTraceLine))
+					.then(
+						ptls => {
+							ptls.forEach(
+								ptl => {
+									//oc.appendLine(`PTL(${ptl.origStr}) -> ${ptl.matchingPath}`);
+									if (ptl.matchingPath) {
+										oc.appendLine(`${ptl.origStr} -> ${ptl.matchingPath}:${ptl.origFL?.line}`);
+									} else {
+										oc.appendLine(ptl.origStr);
+									}
+								}
+							);
+						}
+					);
 
 				oc.show(true);
 			}));
